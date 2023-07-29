7c2458867b50c1a4064861a9cf1634af03c5c9d3
anderson92zolis <azolis92@hotmail.es>
2023-07-29 15:35:09 +0200

    addding the kata 12.2 objectOrientedPrograming

--- ObjectOrientedProgramming/src/realWorldEntityPerson/Main.java
+++ ObjectOrientedProgramming/src/realWorldEntityPerson/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("12.2) Create classes to represent real-world entities (e.g., Person, Animal, Car) with appropriate properties and methods.");
-        Person person = new Person("1", "Scaler",Gender.MALE, (byte) 21);
-        person.printData();
+        Person person1 = new Person("1", "Scaler",Gender.MALE, (byte) 21);
+        person1.printData();
+        Person person2 = new Person("2", "Juana",Gender.FEMALE, (byte) 30);
+        person2.printData();
     }
+
 }
