4fa2f47ef6d086283df37ee762d13e47f41b16c8
anderson92zolis <azolis92@hotmail.es>
2023-08-02 11:17:43 +0200

    14.3) Perform operations such as sum, average, or maximum on a list using streams and lambdas.

--- LambdasFunctionalPrograming/src/main/java/org/example/Main.java
+++ LambdasFunctionalPrograming/src/main/java/org/example/Main.java
@@ -11,11 +11,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("14) Filter a list of objects based on a specific condition using lambdas.");
-        Customer john = new Customer("John P.", 15);
-        Customer sarah = new Customer("Sarah M.", 200);
-        Customer charles = new Customer("Charles B.", 150);
-        Customer mary = new Customer("Mary T.", 1);
+        System.out.println("14.1) Filter a list of objects based on a specific condition using lambdas.");
+        Customer john = new Customer("John P.", 35,50);
+        Customer sarah = new Customer("Sarah M.", 18,200);
+        Customer charles = new Customer("Charles B.", 32,150);
+        Customer mary = new Customer("Mary T.", 45,1);
 
 
         List<Customer> customers = Arrays.asList(john, sarah, charles, mary);
@@ -28,13 +28,34 @@ public class Main {
 
         System.out.println("14.2) Map elements of a list to a new list using a lambda expression.");
 
-        List<String> filterNameByNames= customers
+        List<String> ListByNames= customers
                 .stream()
                 .map(c->c.getName())
                 .collect(Collectors.toList());
 
-        System.out.println(filterNameByNames);
+        System.out.println(ListByNames);
 
-        }
+        System.out.println("14.3) Perform operations such as sum, average, or maximum on a list using streams and lambdas.");
 
+        Integer sum = customers
+                .stream()
+                .map(c->c.getPoints())
+                .collect(Collectors.summingInt(Integer::intValue));
+
+        System.out.println("The sum is of point are: "+sum);
+
+        double average = customers
+                .stream()
+                .mapToInt((a) -> a.getAge())
+                .average().orElse(0);
+
+        System.out.println("The average of ages are: "+average);
+
+        double maximun = customers
+                .stream()
+                .mapToInt(a -> a.getAge())
+                .max().orElse(0);
+
+        System.out.println("The maximum of ages are: "+maximun);
+    }
 }
